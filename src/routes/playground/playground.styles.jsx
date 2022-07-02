d6a15ddb233a4461f9af9304b12aa8445fa674e7
Jared Taylor <jaredalextaylor@gmail.com>
2022-07-02 18:32:11 -0400

    added spinner

--- src/routes/playground/playground.styles.jsx
+++ src/routes/playground/playground.styles.jsx
@@ -1,10 +1,16 @@
 import styled from 'styled-components';
 
 export const PageContainer = styled.div`
-  margin: 0px 2%;
+    margin: 0px 2%;
+`;
+
+export const PlaygroundContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 `;
 
 export const Title = styled.h2`
-  font-size: 38px;
-  text-align: center;
+    font-size: 38px;
+    text-align: center;
 `;

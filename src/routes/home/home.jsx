12d44450e44aadd773d39d6bfa088703329c8cea
Jared Taylor <jaredalextaylor@gmail.com>
2022-07-02 18:58:31 -0400

    added biography to home page

--- src/routes/home/home.jsx
+++ src/routes/home/home.jsx
@@ -1,9 +1,18 @@
-import { PageContainer, Title } from './home.styles';
+import { PageContainer, Title, BioContainer, ImageContainer, ParagraphContainer, BioTitle, BioParagraph } from './home.styles';
 
 const Home = () => {
     return (
         <PageContainer>
             <Title>Welcome!</Title>
+            <BioContainer>
+                <ImageContainer>
+                    <img src='/bio-image.jpg' alt='headshot photo' />
+                </ImageContainer>
+                <ParagraphContainer>
+                    <BioTitle>Hi, my name is Jared</BioTitle>
+                    <BioParagraph>About me.</BioParagraph>
+                </ParagraphContainer>
+            </BioContainer>
         </PageContainer>
     );
 };
--- src/routes/home/home.styles.jsx
+++ src/routes/home/home.styles.jsx
@@ -8,3 +8,34 @@ export const Title = styled.h2`
   font-size: 38px;
   text-align: center;
 `;
+
+export const BioContainer = styled.div`
+    display: flex;
+    background-color: #5555d1;
+    border-radius: 10px;
+`;
+
+export const ParagraphContainer = styled.div`
+    width: 66%;
+    margin: 20px 20px 20px 0px;
+`;
+
+export const BioTitle = styled.h3`
+    background-color: yellow;
+    margin-bottom: 10px;
+`;
+
+export const BioParagraph = styled.p`
+    background-color: pink;
+`;
+
+export const ImageContainer = styled.div`
+    width: 33%;
+    height: 50%;
+    margin: 20px;
+    img {
+        height: 100%;
+        width: 100%;
+        border-radius: 50%;
+    }
+`;

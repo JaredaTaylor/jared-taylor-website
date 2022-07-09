38e644c601d70a8efa8476449a39406e91ac1e5f
Jared Taylor <jaredalextaylor@gmail.com>
2022-07-08 21:21:43 -0400

    fixed resizing

--- src/routes/nav/nav.styles.jsx
+++ src/routes/nav/nav.styles.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
 export const NavContainer = styled.div`
-  height: 70px;
+  padding: 10px 0px;
   width: 100%;
   display: flex;
   justify-content: space-between;

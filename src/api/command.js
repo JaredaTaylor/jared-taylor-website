5f59073633cc47f00ba6f3cd5801d2d790350f9a
jaredataylor <jaredalextaylor@gmail.com>
2025-06-02 22:48:23 -0400

    Update env vars

--- src/api/command.js
+++ src/api/command.js
@@ -1,11 +1,12 @@
-const API_BASE_URL = import.meta.env.API_URL;
+const API_BASE_URL = import.meta.env.VITE_API_URL;
+const API_SECRET = import.meta.env.VITE_API_SECRET;
 
 export async function executeCommand(command, pwd, args = []) {
   const response = await fetch(`${API_BASE_URL}/v1/exec/${command}/`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      'Authentication': 'Api-Key' + import.meta.env.API_SECRET,
+      'Authentication': `Api-Key ${API_SECRET}`,
     },
     body: JSON.stringify({ pwd, args }),
   });

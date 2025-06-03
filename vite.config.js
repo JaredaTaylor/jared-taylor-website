084ed2014ea4442fb7c6691848ee2b967aa3404b
jaredataylor <jaredalextaylor@gmail.com>
2025-06-02 22:14:29 -0400

    Remove old polyfill

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import NodePolyfills from 'vite-plugin-node-polyfills'
+import rollupNodePolyFill from 'rollup-plugin-node-polyfills'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    NodePolyfills({
-      protocolImports: true,
-    }),
-  ],
-  server: {
-    host: true,
-    port: 3000,
-    strictPort: true,
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      plugins: [rollupNodePolyFill()],
+    },
   },
   resolve: {
     alias: {
       crypto: 'crypto-browserify',
+      stream: 'stream-browserify',
+      buffer: 'buffer/',
     },
   },
   define: {
     global: 'globalThis',
+  },
+  server: {
+    host: true,
+    port: 3000,
+    strictPort: true,
   },
 })
